2684782bf4cf43c131062cbc3bc22440a5a4e646
xPabloxz <pablodbandeira@hotmail.com>
2022-01-27 12:08:17 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello ";
+            string b = "World!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
