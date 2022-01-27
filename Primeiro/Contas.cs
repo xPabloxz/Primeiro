e0f2ee53266d77655412c0e7696487e934390404
xPabloxz <pablodbandeira@hotmail.com>
2022-01-27 14:28:15 -0300

    Com testes

---
+++ Primeiro/Contas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contas
+{
+    public class Contas
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+        public int Divisao(int a, int b) 
+        {
+            return a / b;
+        }
+        public int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
