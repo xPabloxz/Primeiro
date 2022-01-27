62b2aabd3c73ca7c096c8f2d71f156fddb7961e3
xPabloxz <pablodbandeira@hotmail.com>
2022-01-27 14:49:36 -0300

    Novo

--- Primeiro/Contas.cs
+++ Primeiro/Contas.cs
@@ -1,28 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Contas
-{
-    public class Contas
-    {
-        public int Soma(int a, int b)
-        {
-            return a + b;
-        }
-        public int Subtracao(int a, int b)
-        {
-            return a - b;
-        }
-        public int Divisao(int a, int b) 
-        {
-            return a / b;
-        }
-        public int Multiplicacao(int a, int b)
-        {
-            return a * b;
-        }
-    }
-}
+﻿//Link abaixo para aprender mais
+//https://docs.microsoft.com/pt-br/visualstudio/windows/?view=vs-2019namespace Contas{    public class Contas    {        public int Soma(int a, int b)        {            return a + b;        }        public int Subtracao(int a, int b)        {            return a - b;        }        public int Divisao(int a, int b)        {            return a / b;        }        public int Multiplicacao(int a, int b)        {            return a * b;        }    }}
